5fd7572bb8e8520a9191c1e705ea159ba7d70f9c
Crowdbotics <team@crowdbotics.com>
2022-03-15 14:10:59 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -6,6 +6,7 @@ const styles = StyleSheet.create({
     borderBottomColor: "black",
     borderBottomWidth: 1
   },
+
   container: { flex: 1, height: 100, padding: 13 },
   text: { color: "black", fontSize: 20 },
   buttonPressed: { backgroundColor: "aquamarine" },
@@ -25,5 +26,17 @@ export const modulesOptions = {
     title: "App Menu",
     copy: "Routes available!",
     styles: styles
+  },
+  "@modules/login": {
+    LOGO_URL:
+      "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/cb-icon.png",
+    BACKGROUND_URL:
+      "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/halfbg.png",
+    HOME_SCREEN_NAME: "Home Screen",
+    validateEmail: "^[^\\s]+([.-]?\\w+)*@\\w+([.-]?\\w+)*(.\\w{2,3})+$",
+    SignInNavText: "Sign In",
+    SignUpNavText: "Sign Up",
+    SignInButtonText: "Login",
+    SignUpButtonText: "Sign Up"
   }
 }
